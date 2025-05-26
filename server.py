e3ec9ffc0094a01ff8eb8b21d4940a22ca7de649
TamaraRozman <150361797+TamaraRozman@users.noreply.github.com>
2025-05-26 22:26:54 +0200

    Update server.py

--- server.py
+++ server.py
@@ -3,13 +3,14 @@ from fastapi.responses import FileResponse
 import os
 import torchaudio
 from speechbrain.inference.separation import SepformerSeparation as separator
-import whisper
+from faster_whisper import WhisperModel
 import uuid
+import json
 
 app = FastAPI()
 
 sep_model = separator.from_hparams(source="speechbrain/sepformer-wsj02mix", savedir='pretrained_models/sepformer-wsj02mix')
-asr_model = whisper.load_model("small")
+model = WhisperModel("medium", compute_type="int8")
 
 @app.post("/separate")
 async def separate_audio(file: UploadFile = File(...)):
@@ -26,9 +27,13 @@ async def separate_audio(file: UploadFile = File(...)):
     for i in range(num_sources):
         filename = f"source{i+1}_{uuid.uuid4()}.wav"
         filepath = os.path.join("output", filename)
-        torchaudio.save(filepath, est_sources[:, :, i].detach().cpu(), 8000)
+        torchaudio.save(filepath, est_sources[:, :, i].detach().cpu(), 8000, encoding="PCM_S",
+        bits_per_sample=16)
 
-        transcription = asr_model.transcribe(filepath, language="hr")["text"]
+        transcription = ''
+        segments, info = model.transcribe(filepath, language="hr")
+        for segment in segments:
+            transcription += (segment.text)
 
         results.append({
             "speaker": f"Govornik {i + 1}",
@@ -36,4 +41,6 @@ async def separate_audio(file: UploadFile = File(...)):
         })
 
     print(results)
-    return {"results": results}
+    os.remove(input_path)
+
+    return(json.dumps(results, indent=4)) 
